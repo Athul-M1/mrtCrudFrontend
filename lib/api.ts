f2831bd993fe2f1fa8723736fb20bf164c7b9083
Athul-M1 <athul10881@gmail.com>
2025-05-22 18:58:00 +0530

    my first commit

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import { User, UserFormData } from './types';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000';
+const API_URL = process.env.NEXT_PUBLIC_API_URL 
 
 export async function getUsers(): Promise<User[]> {
   try {
